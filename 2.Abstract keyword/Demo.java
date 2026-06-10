d2ec16592261602ad201a275cb8f038030101037
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-10 12:00:25 +0530

    Abstract keyword 1.1

--- 2.Abstract keyword/Demo.java
+++ 2.Abstract keyword/Demo.java
@@ -1,11 +1,13 @@
             /*  Abstract Keyword  */
 
+// we can't create the object of abstract class
+// There is no compultion of abstract class can have only abstract methods only either it can normal methods or all methods are normal             
+// if a class contain any one abstract methods so that we have to make abstract class 
+// we can create reference of abstract class
 
-class Car{
+ abstract class Car{     
 
-    public void drive() {
-
-    }
+    public abstract void drive();
 
     public void playMusic() {
         System.out.println("Playing music");
@@ -13,15 +15,30 @@ class Car{
 
 }
 
+class WagonR extends Car {
 
+    @Override
+    public void drive() {
+        System.out.println("Driving");
+
+    }
+
+    public void brand() {
+        System.out.println("name is WagonR");
+    }
+
+}
 
 public class Demo {
  
     public static void main(String[] args) {
         
-        Car obj = new Car();
+        Car obj = new WagonR();
         obj.drive();
         obj.playMusic();
+
+        WagonR obj1 = new WagonR();
+        obj1.brand();
     }
    
 }
