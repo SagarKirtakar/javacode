6ad82ed17517b026fbaa37712e966f1333b898dc
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-10 11:39:46 +0530

    Abstract keyword 1.0

---
+++ 2.Abstract keyword/Demo.java
@@ -0,0 +1,27 @@
+            /*  Abstract Keyword  */
+
+
+class Car{
+
+    public void drive() {
+
+    }
+
+    public void playMusic() {
+        System.out.println("Playing music");
+    }
+
+}
+
+
+
+public class Demo {
+ 
+    public static void main(String[] args) {
+        
+        Car obj = new Car();
+        obj.drive();
+        obj.playMusic();
+    }
+   
+}
