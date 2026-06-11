efb607d7c2abf57a48c49b3a2f0f93eb8ace2a77
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-11 11:50:03 +0530

    Exception handling 1.2

--- 6.Exception Handling/DemoException.java
+++ 6.Exception Handling/DemoException.java
@@ -5,11 +5,21 @@ public class DemoException {
     int i,j,k=0;
 
     i=8;
-    j=0;
+    j=2;
+    int a[]  = new int[4];
     try {
     k = i/j;
-    }catch(Exception e) {
-        System.out.println("Cannot Divide by zero..");
+    for(int c=0; c<=4; c++) {
+        a[c] = c+1;
+    }
+
+    for(int value : a) {
+        System.out.println(value);
+    }
+    }catch(ArithmeticException e) {
+        System.out.println("Cannot Divide by zero.."+e.getMessage());
+    }catch(ArrayIndexOutOfBoundsException e) {
+        System.out.println("Maximum number of array size is 4");
     }
     System.out.println(k);
 
