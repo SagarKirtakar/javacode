75b845a39d527c2cf68cb231fabe77bcfa302f00
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-11 11:27:03 +0530

    Exception handling 1.1

---
+++ 6.Exception Handling/DemoException.java
@@ -0,0 +1,17 @@
+public class DemoException {
+    
+  public static void main(String[] args) {
+    
+    int i,j,k=0;
+
+    i=8;
+    j=0;
+    try {
+    k = i/j;
+    }catch(Exception e) {
+        System.out.println("Cannot Divide by zero..");
+    }
+    System.out.println(k);
+
+  }
+}
