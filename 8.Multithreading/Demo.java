4c87fd2552d781cb860e085a1fc53b74a7f0184b
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-06-13 08:49:50 +0530

    Multithreading 1.2

--- 8.Multithreading/Demo.java
+++ 8.Multithreading/Demo.java
@@ -3,11 +3,23 @@
  * @date: 2026-06-12
  */
 
+// What is Thread
+// Multiple Thread
+// Thread prority and sleep
+// Runnable vs Thread
+
 class A extends Thread{
 
     public void run() {
         for(int i = 0; i <= 100; i++) {
             System.out.println("hi");
+
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         }
     }
 }
@@ -17,6 +29,13 @@ class B extends Thread{
     public void run() {
         for(int i = 0; i <= 100; i++) {
             System.out.println("hello");
+
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         }
     }
 }
@@ -27,7 +46,19 @@ public class Demo {
         A a1 = new A();
         B b1 = new B();
 
+        a1.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println(a1.getPriority());
+
+        System.out.println(b1.getPriority());
+
         a1.start();
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         b1.start();
     }
 }
