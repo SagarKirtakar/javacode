7bb914614f28dd607385a39fa6a0588c746209b6
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-07-02 12:16:54 +0530

    wrapper class example 1.0

---
+++ Wrapper Class/Demo.java
@@ -0,0 +1,16 @@
+
+public class Demo {
+    
+    public static void main(String[] args) {
+        
+        int num = 10;
+
+        // Wrapper class Convert primitives into objects
+
+        Integer obj = Integer.valueOf(num);
+
+        System.err.println(obj + 5);
+
+        System.out.println(obj);
+    }
+}
