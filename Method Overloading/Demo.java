1ec0e2e26d2650d7c702b8ca240013eba413a98a
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-07-02 12:16:07 +0530

    method overloading examples 1.0

---
+++ Method Overloading/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+    
+    public static void main(String[] args) {
+        System.out.println("Main method String[] args");
+        
+    }
+
+    public static void main(int[] args) {
+        System.out.println("Main method int[] args");
+    }
+
+    // We can overload main Method
+}
